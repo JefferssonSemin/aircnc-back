598876b7a2432c67556db411bfa81626c29097e0
JefferssonSemin <jeffesemin@hotmail.com>
2019-10-06 18:56:07 -0300

    Adicionado cors e preview de img

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -1,14 +1,25 @@
 const moongose = require('mongoose');
 
-const SpotSchema = new moongose.Schema({
-  thumbnail: String,
-  company: String,
-  price: Number,
-  techs: [String],
-  user: {
-    type: moongose.Schema.Types.ObjectId,
-    ref: 'User',
+const SpotSchema = new moongose.Schema(
+  {
+    thumbnail: String,
+    company: String,
+    price: Number,
+    techs: [String],
+    user: {
+      type: moongose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
   },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+SpotSchema.virtual('thumbnail_url').get(function() {
+  return `http://localhost:3333/files/${this.thumbnail}`;
 });
 
 module.exports = moongose.model('spot', SpotSchema);
